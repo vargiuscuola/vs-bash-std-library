667d6f80f7947330bc39313821f2c0c095901bb9
Stefano <stefano@vargiuscuola.it>
2019-12-27 12:19:25 +0100

    Test GitHub Action

--- prova.sh
+++ prova.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-# @description Print a message23
+# @description Print a message24
 #
 # @example
 #   show_msg "prova"
 #
 # @arg $1 string Message to print
 #
-# @stdout Print the message23
+# @stdout Print the message24
 show_msg() {
 	echo ok
 }
