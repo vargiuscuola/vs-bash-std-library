4fc033ebb340fc2abd060c5285c3cc2a2ecc1534
Stefano <stefano@vargiuscuola.it>
2019-12-30 16:39:18 +0100

    Test GitHub Action genshdoc

--- prova.sh
+++ prova.sh
@@ -3,14 +3,14 @@
 # @file prova.sh
 # @brief Libreria prova.sh
 
-# @description Print a message62
+# @description Print a message63
 #
 # @example
 #   show_msg "prova"
 #
 # @arg $1 string Message to print
 #
-# @stdout Print the message62
+# @stdout Print the message63
 show_msg() {
 	echo ok
 }
