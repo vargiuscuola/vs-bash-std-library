4ef92c0081086c948274ad7affdd633faf2057e2
Stefano <stefano@vargiuscuola.it>
2020-02-25 14:51:46 +0100

    2020-02-25 14:51:45] Update package.sh library

--- package.sh
+++ package.sh
@@ -3,17 +3,17 @@
 
 # @file Library package.sh
 
-[[ "$( uname -a  )" =~ ^MINGW ]] && _std_lib__lib_dir=/c/linux-lib/sh || _std_lib__lib_dir=/lib/sh
+[[ "$( uname -a  )" =~ ^MINGW ]] && package__lib_dir=/c/linux-lib/sh || package__lib_dir=/lib/sh
 
 package_load-from-git() {
     local is_update git_package
 	while [[ $# -gt 0 ]]; do
 		case "$1" in
-			--update) is_update=true ; break ;;
-			*) git_package="$1" ; break ;;
+			--update) is_update=true ; shift ;;
+			*) [[ -n "$git_package" ]] && { echo "[ERROR] Argument error" >&2 ; return 1 ; } ; git_package="$1" ; shift ;;
 		esac
 	done
-    local lib_dir="$_std_lib__lib_dir/$git_package"
+    local lib_dir="$package__lib_dir/$git_package"
     local git_url="https://$git_package"
     
     if [[ ! -d "$lib_dir" ]]; then
@@ -22,14 +22,14 @@ package_load-from-git() {
     fi
     
     [[ ! -d "$lib_dir" ]] && { echo "Missing git repository in $lib_dir" >&2 ; return 1 ; }
-    ! { cd "$lib_dir" && git fsck &>/dev/null; } && { echo "[ERROR] The git repository in $lib_dir is broken" >&2 ; return 1 ; }
-    if [[ is_update == true ]]; then
+    ( cd "$lib_dir" &>/dev/null && git fsck &>/dev/null ) || { echo "[ERROR] The git repository in $lib_dir is broken" >&2 ; return 1 ; }
+    ( cd "$lib_dir" &>/dev/null && git fetch --prune &>/dev/null ) || { echo "[ERROR] Cannot check updates from origin" >&2 ; return 1 ; }
+    if [[ "$is_update" == true ]]; then
         local local_commitid="$(cd "$lib_dir" && git rev-parse master)"
         local remote_commitid="$(cd "$lib_dir" && git rev-parse origin/master)"
         if [[ "$local_commitid" != "$remote_commitid" ]]; then
             echo -n "Updating git repository in $lib_dir... "
-#            git fetch --prune &>/dev/null
-#            git reset --hard "$remote_commitid" && echo "[OK]" || { echo "[ERROR] The git repository cannot be updated" ; return 1 ; }
+            ( cd "$lib_dir" &>/dev/null && git reset --hard "$remote_commitid" ) || { echo "[ERROR] The git repository cannot be updated" ; return 1 ; }
         fi
     fi
     
